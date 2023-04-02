120eb050ab8b3d23671d17cb44a08f33d758fd48
Sebastian Fritzsche <sebifr58@gmx.de>
2023-04-02 19:53:56 +0200

    create api connection

---
+++ main.js
@@ -0,0 +1,12 @@
+async function TradingData() {
+    let response = await fetch("https://data.lemon.markets/v1/instruments?search=tesla", 
+    {
+      headers: { 'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOiJsZW1vbi5tYXJrZXRzIiwiaXNzIjoibGVtb24ubWFya2V0cyIsInN1YiI6InVzcl9yeUdESDIyZmZzRjRHN0YxWXBZRjJid1pZZkJnOGdiTmRQIiwiZXhwIjoxNzExOTUwMjA2LCJpYXQiOjE2ODA0MTQyMDYsImp0aSI6ImFwa19yeUdESDIyaGhtR0Nac1ZIcmpISlhNMjl6bTZyUmJIRndwIiwibW9kZSI6InBhcGVyIn0.7UHPexMnI1sEnVk1f2Nirs7Sk5vejImbKqwv9d-zxiQ' }
+    })
+
+    let result = await response.json();
+
+    console.log(result.results[0].isin);
+}
+
+TradingData();
