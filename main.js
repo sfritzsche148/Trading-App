cb90bc9cbe0f89ff05c39538f883351e774a4ed3
Sebastian Fritzsche <sebifr58@gmx.de>
2023-04-02 20:26:10 +0200

    search

--- main.js
+++ main.js
@@ -1,12 +1,16 @@
-async function TradingData() {
-    let response = await fetch("https://data.lemon.markets/v1/instruments?search=tesla", 
+async function SearchData() {
+
+        let input = document.getElementById('inp').value;
+        console.log(input)
+
+    let response = await fetch("https://data.lemon.markets/v1/instruments?search=" + input, 
     {
       headers: { 'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOiJsZW1vbi5tYXJrZXRzIiwiaXNzIjoibGVtb24ubWFya2V0cyIsInN1YiI6InVzcl9yeUdESDIyZmZzRjRHN0YxWXBZRjJid1pZZkJnOGdiTmRQIiwiZXhwIjoxNzExOTUwMjA2LCJpYXQiOjE2ODA0MTQyMDYsImp0aSI6ImFwa19yeUdESDIyaGhtR0Nac1ZIcmpISlhNMjl6bTZyUmJIRndwIiwibW9kZSI6InBhcGVyIn0.7UHPexMnI1sEnVk1f2Nirs7Sk5vejImbKqwv9d-zxiQ' }
     })
 
     let result = await response.json();
 
-    console.log(result.results[0].isin);
+    document.write(result.results[0].name);
 }
 
-TradingData();
+SearchData();
